b1a654942b420741ab5f3cd0b382e002138d1d87
pepper-box <67093393+pepper-box@users.noreply.github.com>
2020-11-23 21:09:18 +0800

    Initial commit

---
+++ api1.0.py
@@ -0,0 +1,39 @@
+# post请求
+from fastapi import FastAPI
+from pydantic import BaseModel
+import spacy
+nlp = spacy.load("en_core_web_sm")
+app = FastAPI()
+
+
+@app.get('/test/a={a}/b={b}')
+def calculate(a: int=None, b: int=None):
+    c = a + b
+    res = {"res":c}
+    return res
+
+@app.get('/')
+def hello():
+    return "Hello"
+
+class Item(BaseModel):
+    POS: str = None
+
+
+@app.post('/test')
+def calculate(request_data: Item):
+    if request_data.POS:
+        doc =nlp(request_data.POS)
+        result = {}
+        for token in doc:
+            result[token.text] = token.pos_
+        return result
+    return "error"
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app=app,
+                port=8080,
+                workers=1)
